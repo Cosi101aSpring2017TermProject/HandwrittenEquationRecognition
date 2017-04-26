ccdb02653fa458cbca1a962bb319ead2abc68ee6
Alex Hu <alexhubnds@icloud.com>
2017-04-25 20:29:55 -0400

    building(copying) model

--- SymbolClassification.py
+++ SymbolClassification.py
@@ -5,9 +5,9 @@ import tensorflow as tf
 import argparse
 import sys
 import numpy
+from tensorflow.examples.tutorials.mnist import input_data
 
-#fffffff
-#rrrrrr
+do_restore = 0
 
 class ClassificationDictionary:
 
@@ -37,7 +37,7 @@ class ClassificationDictionary:
         if len(self.classification_dict) == 0:
             print("You cannot call get_classification_array(symbol) for now, \n the classification_dict is empty.")
             return numpy.array([])
-        zeros_array = numpy.zeros(len(self.classification_dict))
+        zeros_array = [0.] * len(self.classification_dict)
         index = self.classification_dict[symbol]
         if index is None:
             print("cannot identify given symbol " + symbol)
@@ -47,7 +47,46 @@ class ClassificationDictionary:
             zeros_array[index] = 1.0
             return zeros_array
 
+    def get_classes_number(self):
+        return len(self.classification_dict)
 
+    def convert_mnist(self, mnist_class_array):
+        i = 0
+        for num in mnist_class_array:
+            if num == 1.:
+                return self.get_classification_array(str(i))
+            i += 1
+        return self.get_classification_array("nothing")
+
+
+
+
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
+
+def rot():
+    # TODO: NEED IMPLEMENTATION
+    return []
+
+def stretch():
+    # TODO: NEED IMPLEMENTATION
+    return []
 
 
 def main(_):
@@ -57,11 +96,82 @@ def main(_):
     testing_data = SymbolSegmentor.SymbolSegmentor()
 
 
-    #TODO: DEFINE MODEL
 
+
+    #TODO: DEFINE MODEL
+    number_of_classes = classficationDic.get_classes_number()
+    mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+
+    x = tf.placeholder(tf.float32, [None, 784])
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
+    y_ = tf.placeholder(tf.float32, [None, number_of_classes])
+    W = tf.Variable(tf.zeros([784, number_of_classes]))
+    b = tf.Variable(tf.zeros([number_of_classes]))
+    y = tf.matmul(x, W) + b
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    W_fc2 = weight_variable([1024, number_of_classes])
+    b_fc2 = bias_variable([number_of_classes])
+
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+    # Define loss and optimizer
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    results = tf.argmax(y_conv, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()  # save training
     #TODO: TRAIN
+    if do_restore == 1:
+        saver.restore(sess, './model')
+    else:
+        # Train
+        for i in range(10000):  # 20000
+            batch = mnist.train.next_batch(50)
+            label_batch = [classficationDic.convert_mnist(num_array) for num_array in batch[1]]
+            train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: label_batch, keep_prob: 1.0})
+            train_step.run(feed_dict={x: batch[0], y_: label_batch, keep_prob: 0.5})
+            # rotated_batch = rot(batch[0], label_batch)
+            # train_step.run(feed_dict={x: rotated_batch[0], y_: rotated_batch[1], keep_prob: 0.5})
+            # stretched_batch = rot(batch[0], label_batch)
+            # train_step.run(feed_dict={x: stretched_batch[0], y_: stretched_batch[1], keep_prob: 0.5})
+            if i % 100 == 0:
+                print("step %d, training accuracy %g" % (i, train_accuracy))
+                print(batch[0].shape)
+                #print(label_batch.shape)
+                print(label_batch)
+
+        save_path = saver.save(sess, 'model')
+        print("Model saved in file: %s" % save_path)
+
 
     #TODO: TEST
+
 
 
 
