c0c1c5ab7c8f988c3bff47945e8b753b1d5430ba
JoshJin <jinzexi1994@hotmail.com>
2017-05-03 04:28:05 -0400

    2AM: some idea about symbol segmentation, following progress: 1. contouring should be working fine 2. struggling w/ extracting symbol with contour (instead of the box) 3. currently trying drawContour, only resulted in a mess ... to be updated
    2:50AM:
    At least, the contour is found and extracted... but the image looks ugly... and the program looks messy
    two places to improve:
    1. the current image id of the same size as the original iamge, how can I make it into a smaller image that just fits the contour
    2. the countour seems to be the circumference of a symbol, I need to recover it back to the symbol

--- SymbolSegmentor.py
+++ SymbolSegmentor.py
@@ -10,30 +10,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class SymbolSegmentor:
+
+    def save_image(mun_tmp, img_tmp):
+        string_temp = "test_output_" + str(mun_tmp) + ".png"
+        cv2.imwrite(string_temp, img_tmp)
+        plt.imshow(img_tmp)
+        plt.show()
+
+    # same as in the openCV tutorial
     im_gray = cv2.imread("test_image_for_contour.png", 0)
     plt.imshow(im_gray)
     plt.show()
+
     # convert to grayscale
     im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
     im = 255 - im_gray
+
     # threshold
     ret, im_th = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY_INV)
-    # find contour
+
+    # find contour, ctrs is the contour
     im2, ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # ctr = ctrs[1]
     # ctr = ctrs[4]
     # im3 = np.zeros((128, 1693, 3), np.uint8)
-    a = 0
+    img_num_counter = 0
     for contour in ctrs:
-        im3 = np.zeros((128, 1693, 3), np.uint8)
-        cv2.drawContours(im3, contour, -1, (0, 255, 0), 3)
-        string_base = "test_output_"
-        string_temp = string_base + str(a) + ".png"
-        a = a+1
-        cv2.imwrite(string_temp, im3)
-        plt.imshow(im3)
-        plt.show()
+        symbol_container = np.zeros((128, 1693, 3), np.uint8)
+        cv2.drawContours(symbol_container, contour, -1, (0, 255, 0), 3)
+        save_image(img_num_counter, symbol_container)
+        img_num_counter = img_num_counter + 1
+
 
     # plt.imshow(ctrs)
     # plt.show()
