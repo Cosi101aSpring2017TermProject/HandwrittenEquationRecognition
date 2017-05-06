9ab529e75f13a55675d11fda824d77dc1cab8aab
rozoalex <alexhubnds@icloud.com>
2017-05-06 02:30:13 -0400

    save imgs

--- SymbolClassification.py
+++ SymbolClassification.py
@@ -200,7 +200,7 @@ def main(_):
     sess.run(tf.global_variables_initializer())
     saver = tf.train.Saver()  # save training
     #TODO: TRAIN
-    if do_restore == 1:
+    if do_restore == 0:
         saver.restore(sess, './model')
     else:
         # Train
--- SymbolSegmentor.py
+++ SymbolSegmentor.py
@@ -8,90 +8,105 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from os import listdir
+import os
+import skimage.io
+import re
 
 class SymbolSegmentor:
 
-    def save_image(mun_tmp, img_tmp):
-        string_temp = "test_output_" + str(mun_tmp) + ".png"
-        cv2.imwrite(string_temp, img_tmp)
+    show_imgs = False
+
+    def save_image(self, img_filename, img_tmp):
+        print("save " + img_filename)
+        os.chdir('symbols')
+        # skimage.io.imsave(img_filename, img_tmp/255)
+        cv2.imwrite(img_filename, img_tmp)
         # plt.imshow(img_tmp)
         # plt.show()
 
-    # same as in the openCV tutorial
-    im_gray = cv2.imread("test_image_for_contour.png", 0)
-    plt.imshow(im_gray)
-    plt.show()
-
-    # convert to grayscale
-    im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
-    # im_gray2 = cv2.GaussianBlur(im_gray, (5, 5), 0)
-    # im_gray3 = cv2.GaussianBlur(im_gray, (7, 7), 0)
-
-    im = 255 - im_gray
-    plt.imshow(im_gray)
-    plt.show()
-
-    # threshold
-    ret, im_th = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY_INV)
-    # ret2, im_th2 = cv2.threshold(im_th, 127, 255, cv2.THRESH_BINARY_INV)
-    # ret3, im_th3 = cv2.threshold(im_th2, 127, 255, cv2.THRESH_BINARY_INV)
-
-    plt.imshow(im_th)
-    plt.show()
-    # find contour, ctrs is the contour
-    im2, ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # ctr = ctrs[1]
-    # ctr = ctrs[4]
-    # im3 = np.zeros((128, 1693, 3), np.uint8)
-    rects = [cv2.boundingRect(ctr) for ctr in ctrs]
-    # zero = 0
-    img_num_counter = 0
-    rect_counter = 0
-    for contour in ctrs:
-        symbol_container = np.zeros((128, 1693, 3), np.uint8)
-        cv2.drawContours(symbol_container, contour, -1, (0, 255, 0), 3)
-        rect = rects[rect_counter]
-        rect_counter = rect_counter + 1
-        # cv2.rectangle(symbol_container, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
-        # print(rect[0])
-        # (rect[1])
-        # print(rect[2])
-        # print(rect[3])
-        vertex_x_begin=rect[0]-10
-        vertex_x_end=rect[0]+ rect[2]+10
-        vertex_y_begin=rect[1]-10
-        vertex_y_end=rect[1]+ rect[3]+10
-        print(vertex_x_end-vertex_x_begin)
-        print(vertex_y_end - vertex_y_begin)
-        crop_img = symbol_container[vertex_y_begin:vertex_y_end, vertex_x_begin:vertex_x_end]
-        # print()
-        # for i in range(0, vertex_x_end-vertex_x_begin):
-        #    for j in range(0, vertex_y_end - vertex_y_begin):
-        #        int_tmp = cv2.pointPolygonTest(contour, (i,j), False)
-        #        if int_tmp == 0:
-        #            crop_img[j][i] = (0, 255, 0)
-        plt.imshow(crop_img)
-        plt.show()
-        save_image(img_num_counter, crop_img)
-        img_num_counter = img_num_counter + 1
-    # rect_counter = zero
+    def segment(self, img_folder_dir, file_name):
+        full_dir = img_folder_dir + file_name
+        im_gray = cv2.imread(full_dir, 0)
+        if self.show_imgs:
+            plt.imshow(im_gray)
+            plt.show()
 
 
-    # plt.imshow(ctrs)
-    # plt.show()
-    for rect in rects:
-        cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
-    im = 255 - im
-    plt.imshow(im)
-    plt.show()
-    cv2.imwrite('test_output.png', im)
+        # convert to grayscale
+        im_gray = cv2.blur(im_gray, (5, 5), 2)
+        # im_gray = cv2.bilateralFilter(im_gray, 5, 7, 7)
+        # im_gray2 = cv2.GaussianBlur(im_gray, (5, 5), 0)
+        # im_gray3 = cv2.GaussianBlur(im_gray, (7, 7), 0)
 
-    # raw_imgs = []
-    # img_boxes = []
+        im = 255 - im_gray
+        if self.show_imgs:
+            plt.imshow(im_gray)
+            plt.show()
+
+        # threshold
+        ret, im_th = cv2.threshold(im, 200, 255, cv2.THRESH_BINARY_INV)
+        # ret2, im_th2 = cv2.threshold(im_th, 127, 255, cv2.THRESH_BINARY_INV)
+        # ret3, im_th3 = cv2.threshold(im_th2, 127, 255, cv2.THRESH_BINARY_INV)
+
+        if self.show_imgs:
+            plt.imshow(im_th)
+            plt.show()
+        # find contour, ctrs is the contour
+        im2, ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # ctr = ctrs[1]
+        # ctr = ctrs[4]
+        # im3 = np.zeros((128, 1693, 3), np.uint8)
+        rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+        # zero = 0
+        img_num_counter = 0
+        rect_counter = 0
+        for contour in ctrs:
+            symbol_container = np.zeros((128, 1693, 3), np.uint8)
+            cv2.drawContours(symbol_container, contour, -1, (0, 255, 0), 3)
+            rect = rects[rect_counter]
+            rect_counter = rect_counter + 1
+            # cv2.rectangle(symbol_container, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
+            # print(rect[0])
+            # (rect[1])
+            # print(rect[2])
+            # print(rect[3])
+            vertex_x_begin = rect[0] - 10
+            vertex_x_end = rect[0] + rect[2] + 10
+            vertex_y_begin = rect[1] - 10
+            vertex_y_end = rect[1] + rect[3] + 10
+            print(vertex_x_end - vertex_x_begin)
+            print(vertex_y_end - vertex_y_begin)
+            crop_img = symbol_container[vertex_y_begin:vertex_y_end, vertex_x_begin:vertex_x_end]
+            # print()
+            # for i in range(0, vertex_x_end-vertex_x_begin):
+            #    for j in range(0, vertex_y_end - vertex_y_begin):
+            #        int_tmp = cv2.pointPolygonTest(contour, (i,j), False)
+            #        if int_tmp == 0:
+            #            crop_img[j][i] = (0, 255, 0)
+            if self.show_imgs:
+                plt.imshow(crop_img)
+                plt.show()
+            rec_string = str(vertex_y_begin)+'_'+str(vertex_y_end)+'_'+str(vertex_x_begin)+'_'+str(vertex_x_end)
+            self.save_image(file_name.replace('.png', '_??_'+rec_string+'.png'), crop_img)
+            img_num_counter = img_num_counter + 1
+            # rect_counter = zero
+            # plt.imshow(ctrs)
+            # plt.show()
+
+        for rect in rects:
+            cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
+        im = 255 - im
+        # plt.imshow(im)
+        # plt.show()
+
     def __init__(self):
-        raw_imgs = []
-        img_boxes = []
-        #TODO: read all equations in annotated folder and drawing boxes around them recode the position of the box in self.img_boxes
-        #TODO: put all images of single symbol in raw_imgs, NO processing needed.
+        print("start segmenting equation pics in annotated folder")
+        files = [f for f in listdir('./annotated')]
+        for f in files:
+            a = f.split("_")
+            if len(a) == 3:
+                print(f)
+                self.segment('./annotated/', f)
 
