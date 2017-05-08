8a6fc57ff7743008f170fe787a70f64fa614e25d
rozoalex <alexhubnds@icloud.com>
2017-05-08 16:26:19 -0400

    image processing: fix bugs

--- SymbolSegmentor.py
+++ SymbolSegmentor.py
@@ -18,28 +18,13 @@ import numpy
 class SymbolSegmentor:
 
     show_imgs = False
+    ignored_pixel_limit = 20
 
     def get_folder(self, full_dir: str):
         print(os.sep)
         dir_component = full_dir.split(os.sep)
         print(dir_component[len(dir_component) - 1])
         return dir_component[len(dir_component) - 1]
-
-    def save_image(self, folder, img_filename, postfix, img_tmp):
-        # full_filename = img_filename.replace('.png', postfix + '.png')
-        # full_path = os.path.join('\symbols', full_filename)
-        # if folder != self.get_folder(os.getcwd()):
-        #     if not os.path.exists(os.getcwd() + os.sep + folder):
-        #         os.makedirs(os.getcwd() + os.sep + folder)
-        #     os.chdir(folder)
-        # print("save " + full_filename + '\nat ' + os.getcwd())
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        # dir_new = folder + '/' + full_filename
-        # skimage.io.imsave(os.getcwd()+'/'+folder+'/'+img_filename, img_tmp)
-        cv2.imwrite(img_filename, img_tmp)
-        # plt.imshow(img_tmp)
-        # plt.show()
 
     def trimZeros(self, two_d_array_img):
         x1 = 0
@@ -66,32 +51,32 @@ class SymbolSegmentor:
                         y2 = y
 
         trimmed_image = two_d_array_img[y1:y2, x1:x2]
-        if self.show_imgs:
-            print(trimmed_image)
-            print("x1:%d y1:%d x2:%d y2:%d" % (x1, y1, x2, y2))
-            plt.imshow(trimmed_image)
-            plt.show()
+        # if self.show_imgs:
+        #     print(trimmed_image)
+        #     print("x1:%d y1:%d x2:%d y2:%d" % (x1, y1, x2, y2))
+        #     plt.imshow(trimmed_image)
+        #     plt.show()
         return x1, y1, x2, y2, trimmed_image
 
     def segment(self, img_folder_dir, file_name):
         full_dir = img_folder_dir + file_name
         im_gray = cv2.imread(full_dir, 0)
         raw_image = cv2.imread(full_dir, 0)
+        img_with_box = cv2.imread(full_dir, 0)
         if self.show_imgs:
             plt.imshow(im_gray)
+            plt.title("raw image: "+file_name)
             plt.show()
 
 
         # convert to grayscale
         im_gray = cv2.blur(im_gray, (3, 3), 2)
-        # blurred_raw = cv2.blur(raw_image, (3, 3), 2)
-        # im_gray = cv2.bilateralFilter(im_gray, 5, 7, 7)
-        # im_gray2 = cv2.GaussianBlur(im_gray, (5, 5), 0)
-        # im_gray3 = cv2.GaussianBlur(im_gray, (7, 7), 0)
 
         im = 255 - im_gray
         if self.show_imgs:
+            print(im_gray)
             plt.imshow(im_gray)
+            plt.title("blurred image: " + file_name)
             plt.show()
 
         # threshold
@@ -102,33 +87,44 @@ class SymbolSegmentor:
         # im2, ctrs, hier = cv2.findContours(im_gray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #openCV
         connected_regions, num_of_labels = skimage.measure.label(im_th, connectivity=5, neighbors=8, background=0, return_num=True)
         print(str(num_of_labels)+" regions found in "+file_name)
+
         if self.show_imgs:
             plt.imshow(im_th)
+            plt.title("threshold blurred image: " + file_name)
             plt.show()
             print(connected_regions)
             plt.imshow(connected_regions)
+            plt.title(str(num_of_labels)+" connected regions")
             plt.show()
         for i in range(1, num_of_labels + 1):
             testimage = (connected_regions == i) * 200
             if self.show_imgs:
                 plt.imshow(testimage)
+                plt.title("symbol "+str(i))
                 plt.show()
             x1, y1, x2, y2, symbol_img = self.trimZeros(testimage)
+            cv2.rectangle(img_with_box, (x1, y1), (x2, y2), (255, 0, 0), 1)
             rec_string = str(y1) + '_' + str(y2) + '_' + str(x1) + '_' + str(x2)
             # MARK: IO
             if not os.path.exists("symbols"):
                 os.makedirs("symbols")
             saved_name = "symbols/" + file_name.replace('.png', '_unclassified_' + rec_string + '.png')
+
+            # MARK: Double check to see if there are inner image of the given region
             trimmed_img = im_th[y1:y2, x1:x2]
             inners, numer_of_inner = skimage.measure.label(trimmed_img, connectivity=5,
                                                            neighbors=8, background=0, return_num=True)
+
             if numer_of_inner == 1:
-                if self.show_imgs:
-                    plt.imshow(raw_image[y1:y2, x1:x2])
-                    plt.show()
-                if (y2-y1)*(x2-x1) > 10:
+                if (y2-y1)*(x2-x1) > self.ignored_pixel_limit:
+                    if self.show_imgs:
+                        plt.imshow(raw_image[y1:y2, x1:x2])
+                        plt.title("symbol " + str(i) + " clopped from raw image")
+                        plt.show()
+
                     cv2.imwrite(saved_name, raw_image[y1:y2, x1:x2])
             else:
+                print("found "+str(numer_of_inner)+" regions in this symbol, which means it has inner symbol(s)")
                 temp_x1 = 0
                 temp_y1 = 0
                 temp_x2 = 0
@@ -136,8 +132,16 @@ class SymbolSegmentor:
                 temp_image = []
                 is_first = True
                 for j in range(1, numer_of_inner + 1):
-                    inner = (connected_regions == j) * 200
+                    inner = (inners == j) * 200
+                    if self.show_imgs:
+                        plt.imshow(inner)
+                        plt.title("inner " + str(j))
+                        plt.show()
                     x1_in, y1_in, x2_in, y2_in, trimmed_inner = self.trimZeros(inner)
+                    if self.show_imgs:
+                        plt.imshow(trimmed_inner)
+                        plt.title("trimmed inner " + str(j))
+                        plt.show()
                     if is_first:
                         temp_x1 = x1_in
                         temp_x2 = x2_in
@@ -153,9 +157,23 @@ class SymbolSegmentor:
                         temp_y1 = y1_in
                         temp_y2 = y2_in
                         temp_image = trimmed_inner
-                if (temp_x2-temp_x1)*(temp_y2-temp_y1) > 10:
-                    print(temp_image)
+                if (temp_x2-temp_x1)*(temp_y2-temp_y1) > self.ignored_pixel_limit:
+                    if self.show_imgs:
+                        print("save outer image " + saved_name + ":")
+                        print(temp_image)
+                        print("\n")
+                        plt.imshow(numpy.array(temp_image))
+                        plt.title("outer in symbol " + str(i))
+                        plt.show()
                     cv2.imwrite(saved_name, numpy.array(temp_image))
+        if not os.path.exists("boxed"):
+            os.makedirs("boxed")
+        boxed_img_path = "boxed/" + file_name
+        if self.show_imgs:
+            plt.imshow(img_with_box)
+            plt.title(boxed_img_path)
+            plt.show()
+        cv2.imwrite(boxed_img_path, img_with_box)
 
 
 
@@ -201,11 +219,21 @@ class SymbolSegmentor:
         # # plt.imshow(im)
         # # plt.show()
 
+    @staticmethod
+    def clear(folder_name):  # delete all files in given dir, if the dir does not exist, create it
+        path_name = "./"+folder_name
+        print("clear all files in folder: "+folder_name)
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+        else:
+            for previously_saved_symbol in [f for f in listdir(path_name)]:
+                os.remove(path_name+"/"+previously_saved_symbol)
 
     def __init__(self):
         print("start segmenting equation pics in annotated folder")
         files = [f for f in listdir('./annotated')]
-
+        self.clear("boxed")
+        self.clear("symbols")
         for f in files:
             a = f.split("_")
             if len(a) == 3:
